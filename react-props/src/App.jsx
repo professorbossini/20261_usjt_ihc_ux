a0e7a55aca19d3b51fc490cb77ddc7bb2c0ab2de
Rodrigo Bossini <professorbossini@gmail.com>
2026-04-17 08:53:59 -0300

    react: props icone hippo e bootstrap

---
+++ react-props/src/App.jsx
@@ -0,0 +1,11 @@
+const App = () => {
+  return <div className="container border rounded mt-3">
+    <div className="row">
+      <div className="col-12">
+        <i className="fa-solid fa-hippo"></i>
+      </div>
+    </div>
+  </div>
+}
+
+export default App
