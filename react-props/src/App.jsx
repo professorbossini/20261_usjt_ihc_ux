37b95cffca88b539174241c2e541a85b04e732c0
Rodrigo Bossini <professorbossini@gmail.com>
2026-04-23 21:36:01 -0300

    props: exercicio card proposto, resolver

--- react-props/src/App.jsx
+++ react-props/src/App.jsx
@@ -5,6 +5,19 @@ const App = () => {
         <i className="fa-solid fa-hippo"></i>
       </div>
     </div>
+    <div className="row">
+      <div className="col-12">
+        <h1 className="display-5 text-center">
+          Seus pedidos
+        </h1>
+      </div>
+    </div>
+    <div className="row">
+      <div className="col-sm-12 col-md-6 col-lg-3">Item 1</div>
+      <div className="col-sm-12 col-md-6 col-lg-3">Item 2</div>
+      <div className="col-sm-12 col-md-6 col-lg-3">Item 3</div>
+      <div className="col-sm-12 col-md-6 col-lg-3">Item 4</div>  
+    </div>
   </div>
 }
 
